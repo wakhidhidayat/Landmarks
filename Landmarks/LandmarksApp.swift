789d3b9641db644661a97c204109a13cc3938c25
Wahid Hidayat <wshidayat38@gmail.com>
2020-12-19 20:56:13 +0700

    first commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Wahid Hidayat on 19/12/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
