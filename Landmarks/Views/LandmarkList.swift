d64842a7b497d6504774f52034f926b186539739
Wahid Hidayat <wshidayat38@gmail.com>
2020-12-20 12:01:19 +0700

    created dynamic list

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Wahid Hidayat on 20/12/20.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
