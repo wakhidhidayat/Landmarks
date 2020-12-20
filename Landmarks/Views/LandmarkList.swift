6c71ce0d4fa752dfdd90c543bce21882a3fcbd5f
Wahid Hidayat <wshidayat38@gmail.com>
2020-12-20 14:47:51 +0700

    added filter the list view

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -8,9 +8,17 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFavoritesOnly = false
+    
+    var filterLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
         NavigationView {
-            List(landmarks) { landmark in
+            List(filterLandmarks) { landmark in
                 NavigationLink(
                     destination: LandmarkDetail(landmark: landmark)) {
                     LandmarkRow(landmark: landmark)
